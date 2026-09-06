1ec24052e8757f1877ef574c3b7eb51a6a964607
Mohammad <mmm.2015.ma60@gmail.com>
2026-09-07 01:11:35 +0330

    fix: add missing animation imports to SearchScreen.kt

--- android/app/src/main/java/com/telegramtv/ui/search/SearchScreen.kt
+++ android/app/src/main/java/com/telegramtv/ui/search/SearchScreen.kt
@@ -1,5 +1,8 @@
 package com.telegramtv.ui.search
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
